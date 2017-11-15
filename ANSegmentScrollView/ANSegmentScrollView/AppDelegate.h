3834b6674d63081b03e80e0049da54bd6de17ca2
AudiebantNil <audiebantnil@gmail.com>
2017-11-15 11:02:18 +0800

    新建工程

---
+++ ANSegmentScrollView/ANSegmentScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ANSegmentScrollView
+//
+//  Created by AudiebantNil on 2017/11/15.
+//  Copyright © 2017年 AudiebantNil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ANSegmentScrollView/ANSegmentScrollView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ANSegmentScrollView
+//
+//  Created by AudiebantNil on 2017/11/15.
+//  Copyright © 2017年 AudiebantNil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
